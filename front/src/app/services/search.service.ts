ea77de0efabbde4b7dc49a6b2b0325b4b08f7971
Gabriele Jandres <gabrielejandres@gmail.com>
2020-07-06 18:55:14 -0300

    integração listar todas as repúblicas

--- front/src/app/services/search.service.ts
+++ front/src/app/services/search.service.ts
@@ -11,48 +11,48 @@ export class SearchService {
 
   constructor(public http: HttpClient) { }
 
-  //Lista as repúblicas de um bairro
-	getRepublicsByNeighborhood(neighborhood: string):Observable<any>{
+  // Lista as repúblicas de um bairro
+	getRepublicsByNeighborhood(neighborhood: string): Observable<any>{
 		return this.http.get(this.apiURL + 'searchNeighborhood/' + neighborhood);
   }
 
-  //Listar todas as repúblicas do BD - FAZER A FUNÇÃO NO BACK
-  getRepublics():Observable<any>{
+  // Listar todas as repúblicas do BD 
+  getRepublics(): Observable<any>{
 		return this.http.get(this.apiURL + 'listallRepublic');
 	}
 
-  //Lista os dados da república por id
-	getRepublic(id:number):Observable<any>{
+  // Lista os dados da república por id
+	getRepublic(id:number): Observable<any>{
 		return this.http.get(this.apiURL + 'listRepublic/' + id);
   }
 
-  //Lista as repúblicas com os menores preços - triplo
-	getRepublicsByPriceTriple():Observable<any>{
+  // Lista as repúblicas com os menores preços - triplo
+	getRepublicsByPriceTriple(): Observable<any>{
 		return this.http.get(this.apiURL + 'searchPriceTriple');
   }
 
-  //Lista as repúblicas com os menores preços - duplo
-	getRepublicsByPriceDouble():Observable<any>{
+  // Lista as repúblicas com os menores preços - duplo
+	getRepublicsByPriceDouble(): Observable<any>{
 		return this.http.get(this.apiURL + 'searchPriceDouble');
   }
 
-  //Lista as repúblicas com os menores preços - individual
-	getRepublicsByPriceSingle():Observable<any>{
+  // Lista as repúblicas com os menores preços - individual
+	getRepublicsByPriceSingle(): Observable<any>{
 		return this.http.get(this.apiURL + 'searchPriceSingle');
   }
 
-  //Lista as repúblicas com as melhores avaliações
-	getRepublicsByBestEvaluation():Observable<any>{
+  // Lista as repúblicas com as melhores avaliações
+	getRepublicsByBestEvaluation(): Observable<any>{
 		return this.http.get(this.apiURL + 'searchBestEvaluation');
   }
 
-  //Lista o usuário que tem um id
-  getUser(id:number):Observable<any>{
+  // Lista o usuário que tem um id
+  getUser(id:number): Observable<any>{
 		return this.http.get(this.apiURL + 'listUser/' + id);
   }
 
-  //Lista o usuário que está relacionado com uma república
-  getUserByIdRepublic(republic_id:number):Observable<any>{
+  // Lista o usuário que está relacionado com uma república
+  getUserByIdRepublic(republic_id:number): Observable<any>{
 		return this.http.get(this.apiURL + 'listUserByIdRepublic/' + republic_id);
   }
 
